15af33ec60bf4cc22908f571f613e38a904e559f
Skripal9 <skripal9@gmail.com>
2022-09-25 22:50:56 +0200

    start new work app

---
+++ script.js
@@ -0,0 +1,38 @@
+const quizData = [
+    {
+        question: 'How old is Florin?',
+        a: '10',
+        b: '17',
+        c: '26',
+        b: '110',
+        correct:
+    } , {
+        question: 'what is the most used programming language in 2022?',
+        a: 'java',
+        b: 'C',
+        c: 'Python',
+        d: 'JavaScript',
+        correct: 'a'
+    } , {
+        question: 'Who is he President of USA?',
+        a: 'Florin Pop',
+        b: 'Donald Trump',
+        c: 'Ivan Saldano',
+        d: 'Mihai Andrei',
+        correct: 'b'
+    } , {
+        question: 'What does HTML stand for?',
+        a: 'Hypertext Markup Language',
+        b: 'Cascading Style Sheet',
+        c: 'Jason Object Notation',
+        d: 'Helicopters Terminals Motorboats Lamborginis',
+        correct: 'a'
+    } , {
+        question: 'What year was JavaScript Launched?',
+        a: '1996',
+        b: '1995',
+        c: '1994',
+        d: 'none of the above',
+        correct: 'd'   
+    }
+]
